d87bd30a99a9ec01f6f7265c5aa5cb42c4b42b40
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-06-18 20:08:32 +0000

    Completed the Day-45 practice program.

--- 41-50_Program/Day45.py
+++ 41-50_Program/Day45.py
@@ -2,4 +2,17 @@
 # The function should ask the user to input their calories intake for any number of days and 
 # once they hit 'done' it should calculate and return the average intake.
 
- 
+def average_calories():
+    day = 1
+    calorie_count = 0
+    print("Enter 'done' to finish the count.")
+    while True:
+        choice = input(f"Enter Day {day} calorie intake: ")
+        if choice == 'done':
+            break
+        day += 1
+        calorie_count += int(choice)
+
+    return print(f"Average calories intake of {day-1} days is {calorie_count//(day-1)}")
+
+average_calories()
