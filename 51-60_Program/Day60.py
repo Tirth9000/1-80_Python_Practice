d755a326472bce9b18c6d13a87dd9e4b000349d4
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-06-21 15:32:55 +0000

    Completed 5 more practice questions.

--- 51-60_Program/Day60.py
+++ 51-60_Program/Day60.py
@@ -1,4 +1,3 @@
-
 # Write a function called password_validator. The function asks the user to enter a password. 
 # A valid password should have at least one upper letter, one lower letter, and one number. 
 # It should not be less than 8 characters long. When the user enters a password, the function should check if the password is valid. 
